92a51a2752e36ca59d6e6c702ca127c97aa308e5
Steve <stevetuozzo@gmail.com>
2023-10-05 20:37:50 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Add Route(s) here */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
