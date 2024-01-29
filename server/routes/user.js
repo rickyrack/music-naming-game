52ce29c852b175910399b2e9d2ef9f8b43eeeafd
Gildian <gildian@thinkpad.mynetworksettings.com>
2024-01-28 22:50:54 -0500

    Testing merge

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//Testing Merge
+
 // POST Create user
 router.post('/register', async (req, res) => {
     res.status(201).json(req.body);
